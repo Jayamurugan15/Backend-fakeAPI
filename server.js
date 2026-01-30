4b886032bfcbe491c6f8b79d4ea7c4bb2a615486
Jayamurugan subramaniyan <155968257+Jayamurugan15@users.noreply.github.com>
2026-01-30 08:55:22 +0530

    Update CORS origin to include production URL

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const defaultMiddlewares = jsonServer.defaults({
 
 // CORS Configuration
 server.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:5173', '*'],
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:5173', 'https://container-presenter-pattern.vercel.app/'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
